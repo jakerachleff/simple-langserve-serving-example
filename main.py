a21e12fa5e98a67a1970622a9283989ecd33a3ba
Jake Rachleff <jake@langchain.dev>
2023-11-10 11:48:11 -0800

    print out env vars

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 """Main entrypoint for the app."""
+import logging
+import os
+
 from fastapi import FastAPI
 from langchain.chat_models import ChatOpenAI
 from langserve import add_routes
 
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 add_routes(app, ChatOpenAI(), path="/chat")
+
+print("Environment is: ", os.environ)
+logger.info("Environment is: ", os.environ)
 
 if __name__ == "__main__":
     import uvicorn
