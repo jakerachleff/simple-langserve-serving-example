ee0a3d8dcc82630b1b903b917dbbbfae1e99e391
Jake Rachleff <jake@langchain.dev>
2023-11-10 11:50:15 -0800

    lambda path

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ import os
 
 from fastapi import FastAPI
 from langchain.chat_models import ChatOpenAI
+from langchain.schema.runnable import RunnableLambda
 from langserve import add_routes
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI()
-add_routes(app, ChatOpenAI(), path="/chat")
-
+# add_routes(app, ChatOpenAI(), path="/chat")
+add_routes(app, RunnableLambda(lambda x: x), path="/lambda")
 print("Environment is: ", os.environ)
 logger.info("Environment is: ", os.environ)
 
